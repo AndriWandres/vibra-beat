1a1e4bf57e9776b905c955d7136b78b448880c3b
unknown <andrilucawandres@gmail.com>
2018-11-01 07:43:39 +0100

    documentation

--- VibraBeat/app/src/main/java/ch/vibrabeat/silvanandri/vibrabeat/model/Beat.java
+++ VibraBeat/app/src/main/java/ch/vibrabeat/silvanandri/vibrabeat/model/Beat.java
@@ -2,37 +2,78 @@ package ch.vibrabeat.silvanandri.vibrabeat.model;
 
 import com.orm.SugarRecord;
 
+/**
+ * Represents a beat in VibraBeat
+ */
 public class Beat extends SugarRecord {
+    /** Name of the beat */
     private String name;
+
+    /**
+     * Rhythm pattern of the beat.
+     * Displayed as milliseconds separated with semicolons.
+     */
     private String beatString;
 
+    /** Creates a new beat */
     public Beat() { }
 
+    /**
+     * Creates a new beat
+     * @param beatString Rhythm pattern of the beat
+     */
     public Beat(String beatString) {
         this(null, beatString);
     }
 
+    /**
+     * Creates a new beat
+     * @param name Name of the beat
+     * @param beatString Rhythm pattern of the beat
+     */
     public Beat(String name, String beatString) {
         this.name = name;
         this.beatString = beatString;
     }
 
+    /**
+     * Gets the name of the beat
+     * @return The name of the beat
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the beats name
+     * @param name Name to be set
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the rhythm pattern of the beat
+     * @return The rhythm pattern of the beat
+     */
     public String getBeatString() {
         return beatString;
     }
 
+    /**
+     * Set the beats rhythm pattern
+     * @param beatString Rhythm pattern to be set
+     */
     public void setBeatString(String beatString) {
         this.beatString = beatString;
     }
 
+    /**
+     * Calculates the duration of a beat according to its beat string.
+     * The time is displayed as mm:ss
+     *
+     * @return duration of beat in mm:ss format.
+     */
     public String getBeatLength() {
         if (beatString == null || beatString == "") {
             return "00:00";
@@ -59,6 +100,10 @@ public class Beat extends SugarRecord {
         return res;
     }
 
+    /**
+     * Get the string representation of a beat.
+     * @return Beat represented as a string
+     */
     public String toString() {
         return name + " " + beatString;
     }
