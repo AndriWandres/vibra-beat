d1723a3feecf5bf16846ea5e3db191a7d48c25c8
unknown <andrilucawandres@gmail.com>
2018-11-01 11:01:56 +0100

    further documentation

--- VibraBeat/app/src/main/java/ch/vibrabeat/silvanandri/vibrabeat/database/DbManager.java
+++ VibraBeat/app/src/main/java/ch/vibrabeat/silvanandri/vibrabeat/database/DbManager.java
@@ -4,18 +4,29 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
+/**
+ * Manages the database connection
+ */
 public class DbManager {
+    /** Tag for writing debug logs */
     private static final String LOG_TAG = "DB_ACCESS";
 
+    /** SQLiteDatabase instance */
     private SQLiteDatabase database;
+
+    /** DbHelper instance */
     private DbHelper dbHelper;
 
+    /**
+     * Creates a new DbManager
+     * @param context Information about application environment
+     */
     public DbManager(Context context) {
         Log.d(LOG_TAG, "DbManager creates DbHelper.");
         dbHelper = new DbHelper(context);
     }
 
-
+    /** Opens a database connection */
     public void open() {
         database = dbHelper.getWritableDatabase();
         Log.d(LOG_TAG, "Database reference received. Path to database: " + database.getPath());
